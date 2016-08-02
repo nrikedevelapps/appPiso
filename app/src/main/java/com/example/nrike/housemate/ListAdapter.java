889466075cac3a23a8eda6c4494e3c5dfa0cf1ac
nrike <nrikedevelapps@gmail.com>
2016-08-02 08:03:39 +0200

    primer commit

---
+++ app/src/main/java/com/example/nrike/housemate/ListAdapter.java
@@ -0,0 +1,34 @@
+package com.example.nrike.housemate;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by nrike on 1/08/16.
+ */
+public class ListAdapter extends BaseAdapter {
+
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
